a704434f2a1a6a7e61a866a1fda4e532bfc05f6f
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-07 12:08:36 +0530

    hello world

---
+++ script.js
@@ -0,0 +1,15 @@
+//Three Ways To Print Hello World
+
+// console.log()
+//alert !!!
+//document
+
+// console.log()
+console.log("hello world");
+
+//alert 
+alert("Hey this my first hello world program");
+
+
+//document
+document.write("HI Hello! ")
